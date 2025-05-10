11de69cabac2cd94940aacd282dcfb72f3e89dde
Giovanni0788 <davidc11791@gmail.com>
2025-05-09 22:05:25 -0400

    Added Top Icons

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -10,9 +10,42 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import Image from '@tiptap/extension-image'
 import ImageResize from "tiptap-extension-resize-image";
+import { useEditorStore } from '@/store/use-editor-store';
+import Underline from '@tiptap/extension-underline'
+import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style'
+import { Color } from '@tiptap/extension-color'
+import Highlight from '@tiptap/extension-highlight'
+import Link from '@tiptap/extension-link'
 
 export const Editor = () => {
+    const {setEditor} = useEditorStore();
+
     const editor = useEditor({
+        onCreate({editor}) {
+            setEditor (editor);
+        },
+        onDestroy() {
+            setEditor (null);
+        },
+        onUpdate({editor}) {
+            setEditor (editor);
+        },
+        onSelectionUpdate({editor}) {
+            setEditor (editor);
+        },
+        onTransaction({editor}) {
+            setEditor (editor);
+        },
+        onFocus({editor}) {
+            setEditor (editor);
+        },
+        onBlur({editor}) {
+            setEditor (editor);
+        },
+        onContentError({editor}) {
+            setEditor (editor);
+        },
         editorProps: {
             attributes: {
                 style: "padding-left: 56px; padding-right: 65px;",
@@ -21,6 +54,18 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            Link.configure({
+                openOnClick: false,
+                autolink: true,
+                defaultProtocol:"https"
+            }),
+            Color,
+            Highlight.configure({
+                multicolor:true,
+            }),
+            FontFamily,
+            TextStyle,
+            Underline,
             Image,
             ImageResize,
             Table,
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,15 +1,17 @@
 import { Editor } from "./editor";
+import Toolbar from "./toolbar";
+
 
 interface DocumentIdPageProps {
     params: Promise<{documentId: string}>;
 };
 
 const DocumentIdPage = async ({params}: DocumentIdPageProps) => {
-const {documentId} = await params;
+const {} = await params;
 
 return (
     <div className="nin-h-screen bg-[#FAFBFD]">
-
+        <Toolbar />
         <Editor />
     </div>
 );
